ab2562f0c1de18c504e242d674caf36907b52a91
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-08-12 13:36:18 +0000

    girls who code template

---
+++ script.js
@@ -0,0 +1,33 @@
+let enterButton;
+let a1Button;
+let a2Button;
+let b1Button;
+let b2Button;
+let screen = 0;
+
+function setup() {
+    createCanvas(600, 400);
+    textAlign(CENTER);
+    textSize(20);
+    noStroke();
+    background("pink");
+    text(
+        "Welcome to screen 0. This is the home screen.",
+        width/2,
+        height/2-100
+    );
+    // Create buttons for all screens
+}
+
+function draw() {
+    // Display enter button
+
+    // Add A1 button
+
+    // Add A2 button
+
+    // Check enter button
+
+}
+
+// Functions to display screens
