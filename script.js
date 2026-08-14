c6f9c8c66dede94c4348833ed1d9846044fe66eb
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-08-14 00:46:57 +0000

    added two choices in screen 1

--- script.js
+++ script.js
@@ -17,9 +17,48 @@ function setup() {
         height / 2-100
     );
     // Create buttons for all screens
+    enterButton = new Sprite(300, 300, 100, 50, "k");
+    enterButton.color = "plum";
+    enterButton.text = "Enter";
+
+    a1Button = new Sprite(-200, -200, 50, 50, "k");
+
+    a2Button = new Sprite(-50, -50, 50, 50, "k");
+
 }
 
 function draw() {
+    if (enterButton.mouse.presses()) {
+        print("pressed");
+        background("paleturquoise");
+        text (
+            "Welcome to screen 1. Make your first choice.",
+            width / 2,
+            height / 2 - 100
+        );
+        enterButton.pos =  {x: -100, y: -100};
+
+        a1Button.pos = {x: 250, y: 300};
+        a1Button.color = "plum";
+        a1Button.text = "A1";
+
+        a2Button.pos = {x: 350, y:300};
+        a2Button.color = "plum";
+        a2Button.text = "A2";
+
+        screen = 1;
+        
+    }
+
+    if (screen==1){
+        if(a1Button.mouse.presses()) {
+            print("Display screen 2.")
+            screen = 2;
+        } else if (a2Button.mouse.presses()) {
+            print("Display screen 5.")
+            screen = 5;
+        }
+    }
     // Display enter button
 
     // Add A1 button
